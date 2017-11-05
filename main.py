876804cdd1d7e46b219ee903e195297772764566
xiaoyu shen <xshen@ws47lx.lsv.uni-saarland.de>
2017-11-06 00:55:04 +0100

    update main

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ d_size=3
 l_size=1
 lr=0.0002
 em=torch.ones((9,3))
+train_dialogs = [[1,2,3,4,0,5,6,7] for i in range(20)]
+valid_dialogs = [[1,2,3,4,0,5,6,7] for i in range(20)]
 ######################
 
 
@@ -21,6 +23,4 @@ if __name__ == '__main__':
     s=lm(em,d_size,independence=False, lr=lr)
     #s=hred(em,h_size,c_size,d_size,lr)
     #s=vhred(em,h_size,c_size,d_size,l_size,lr)
-    dialogs = [[1,2,3,4,0,5,6,7] for i in range(20)]
-    print(dialogs)
-    s.run_train(dialogs, dialogs, 20, 6)
+    s.run_train(train_dialogs, valid_dialogs, 20, 6)
